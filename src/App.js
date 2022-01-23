ee7cbe7e5c398cfcc1cae2aa994f3adfaff16eff
youichi Ishigaki <deepestred1968@gmail.com>
2022-01-23 16:31:13 +0900

    25.useCallback

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState, useCallback } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AppContext from "./contexts/AppContext";
 
-import Memo from "./components/Memo";
+import CountClick from "./components/CountClick";
+import CountDisplay from "./components/CountDisplay";
 
 // import BasicUseEffect from './components/BasicUseEffect';
 // import Basic2 from './components/Basic2';
@@ -12,6 +13,7 @@ import Memo from "./components/Memo";
 // import BasicReducer from './components/BasicReducer';
 // import B from './components/B';
 // import CompB from "./components/CompB";
+// import Memo from "./components/Memo";
 
 const initialState = 0;
 const reducer = (currentState, action) => {
@@ -30,6 +32,17 @@ const reducer = (currentState, action) => {
 const App = () => {
 	const [count, dispatch] = useReducer(reducer, initialState);
 
+	const [count1, setCount1] = useState(0);
+	const [count2, setCount2] = useState(0);
+
+	const addCount1 = useCallback(() => {
+		setCount1((prevCount1) => prevCount1 + 1);
+	}, [count1]);
+
+	const addCount2 = useCallback(() => {
+		setCount2((prevCount2) => prevCount2 + 1);
+	}, [count2]);
+
 	return (
 		<AppContext.Provider
 			value={{ countProvided: count, dispatchProvided: dispatch }}
@@ -45,7 +58,11 @@ const App = () => {
 					{/* <BasicReducer/> */}
 					{/* <div>Count : {count}</div> */}
 					{/* <CompB /> */}
-          <Memo/>
+					{/* <Memo /> */}
+					<CountDisplay name="count1" count={count1} />
+					<CountClick handleClick={addCount1}>AddCount1</CountClick>
+					<CountDisplay name="count2" count={count2} />
+					<CountClick handleClick={addCount2}>AddCount2</CountClick>
 				</header>
 			</div>
 		</AppContext.Provider>
